2413d48847975f18184a9eab863b7e567ae2266b
Nellie Crocker <crocker.nellie@gmail.com>
2021-07-02 16:19:19 -0400

    added npm Geohash

--- team-app/src/components/TicketMaster.js
+++ team-app/src/components/TicketMaster.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import GeoLocation from './GeoLocation'
+import Geohash from 'latlon-geohash'
 
-const TicketMaster = (props) => {
+const key = "Ep9KcAms4qdlsc2yjWmcMEs1AtOQLrKJ"
+const secret = "FYePGGoK9mq0OEEe"
+
+const url = 'https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey=Ep9KcAms4qdlsc2yjWmcMEs1AtOQLrKJ'
+
+const TicketMaster = () => {
+
+    const eventFetch =() => {
+        fetch(`${url}`)
+        .then(res=> {
+            if (res.status !== 200){
+                throw new Error('Unable to retrieve events')
+            } else return res.json()
+        })
+        .then(json => {
+            if (json.results.length === 0){
+                throw new Error ('No events available')
+            } else {
+                console.log(json.results);
+            }
+        })
+    }
+
+
     return (
-        <div>
-            
+        <div className="main">
+<div className="mainDiv">
+    <button onClick={eventFetch}>Click to find events in your area</button>
+</div>
         </div>
     );
 }
