f3c8af97b2694bec9e228e840be024f32a6b182f
Nellie Crocker <crocker.nellie@gmail.com>
2021-07-01 23:08:59 -0400

    added all components, GeoLocation is working

---
+++ team-app/src/components/GeoLocation.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+
+const GeoLocation = () => {
+
+    const [lat, setLat] = useState(null)
+    const [lng, setLng] = useState(null)
+    const [status, setStatus] = useState(null)
+
+    const getLocation = () => {
+        if (!navigator.geolocation) {
+            setStatus('Geolocation is not supported by your browser');
+        } else {
+            setStatus('Initializing missile target...');
+            navigator.geolocation.getCurrentPosition((position) => {
+                setStatus(null);
+                setLat(position.coords.latitude);
+                setLng(position.coords.longitude);
+            }, () => {
+                setStatus('Unable to retrieve your location');
+            });
+        }
+    }
+
+    return (
+        <div>
+            <button onClick={getLocation}>Get Location</button>
+            <h1>Your Coordinates:</h1>
+            <p>{status}</p>
+            {lat && <p>Latitude: {lat}</p>}
+            {lng && <p>Longitude: {lng}</p>}
+        </div>
+    )
+}
+
+
+
+
+
+export default GeoLocation
