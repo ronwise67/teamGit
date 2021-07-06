a9dab2ff873a1da7b1eaf66674128c21f6aea865
Nellie Crocker <crocker.nellie@gmail.com>
2021-07-06 11:58:14 -0400

    the fetch and map are working! Still trying to get the geolocation to work.

--- team-app/src/App.js
+++ team-app/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 // import GeoLocation from './components/GeoLocation'
 // import Sitebar from './components/Sitebar'
 import TicketMaster from './components/TicketMaster'
+import reactstrap from 'reactstrap'
 
 
 function App() {
--- team-app/src/components/TicketMaster.js
+++ team-app/src/components/TicketMaster.js
@@ -1,71 +1,101 @@
 import React, { useState, useEffect } from 'react';
-// import GeoLocation from './GeoLocation'
-import Geohash from 'latlon-geohash'
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Button, CardDeck
+} from 'reactstrap'
 
 // const key = "Ep9KcAms4qdlsc2yjWmcMEs1AtOQLrKJ"
 // const secret = "FYePGGoK9mq0OEEe"
 
-let geoPoint
-
 //geo specific but getting errors on the fetch
-const customUrl = 'https://app.ticketmaster.com/discovery/v2/venues.json?lat=40.060034883696844long=-86.06601094446212&apikey=Ep9KcAms4qdlsc2yjWmcMEs1AtOQLrKJ'
+const customUrl = 'https://app.ticketmaster.com/discovery/v2/venues.json?&apikey=Ep9KcAms4qdlsc2yjWmcMEs1AtOQLrKJ&latlong="${latLng}'
 
 
 //this fetches successfully but doesn't make events geo specific
-const url=`https://app.ticketmaster.com/discovery/v2/events.json?&source=ticketmaster&apikey=Ep9KcAms4qdlsc2yjWmcMEs1AtOQLrKJ`
 
+const url = `https://app.ticketmaster.com/discovery/v2/events?apikey=Ep9KcAms4qdlsc2yjWmcMEs1AtOQLrKJ`
 
-const GeoLocation2 = () => {
+const TicketMaster = (props) => {
 
-    const geoPoint = Geohash.encode(lat, lng, [9])
+    const [eventName, setEventName] = useState("")
+    const [eventDate, setEventDate] = useState()
+    const [eventImage, setEventImage] = useState()
+    const [eventInfo, setEventInfo] = useState("")
+    const [eventTickets, setEventTickets] = useState()
+    const [eventData, setEventData] = useState([])
+    
+    useEffect(() => {
+        fetch(url) //+endpoint
+            .then(res => res.json())
+            .then(json => setEventData(json._embedded.events))
+    }, [])
 
-    const [lat, setLat] = useState(null)
-    const [lng, setLng] = useState(null)
-    const [status, setStatus] = useState(null)
+    useEffect(()=>{
+        setEventName(eventData.map((e) => e.name))
+        setEventDate(eventData.map((e) => e.dates.start.localDate))
+        setEventImage(eventData.map((e) => e.images[0].url))
+        setEventInfo(eventData.map((e) => e.info))
+        setEventTickets(eventData.map((e) => e.info))
 
-    const getLocation = () => {
-        if (!navigator.geolocation) {
-            setStatus('Geolocation is not supported by your browser');
-        } else {
-            setStatus('Initializing missile target...');
-            navigator.geolocation.getCurrentPosition((position) => {
-                setStatus(null);
-                setLat(position.coords.latitude);
-                setLng(position.coords.longitude);
-            }, () => {
-                setStatus('Unable to retrieve your location');
-            });
-        }
-    }
+        // setEventName(eventData.name)
+        // setEventDate(eventData.dates.start.localDate)
+        // setEventImage(eventData.images[0].url)
+        // setEventInfo(eventData.info)
+        // setEventTickets(eventData.url)
 
-    return (
-        <div>
-            <button onClick={getLocation}>Get Location</button>
-            <h1>Your Coordinates:</h1>
-            <p>{status}</p>
-            {lat && <p>Latitude: {lat}</p>}
-            {lng && <p>Longitude: {lng}</p>}
-            {geoPoint}
-        </div>
-    )
-}
-
-const TicketMaster = () => {
-
-    const getEvents = fetch(customUrl)
-        .then(res => res.json())
-        .then(json => {
-            console.log(json._embedded.events);
-        })
+    }, [eventData])
 
     return (
-        <div className="main">
-            <div className="mainDiv">
-                {/* <button onClick={getEvents}>Click to find events in your area</button> */}
-            </div>
-        </div>
+        <CardDeck style={{ display: 'flex', flexDirection: 'row', width: '100%', justifyContent: 'space-between' }}>
+            <Card style={{ flex: 2, margin: '10px' }}>
+                <CardImg top width="25%" src={eventImage[1]} alt="Card image cap" />
+                <CardBody>
+                    <CardTitle tag="h2">{eventName[1]}</CardTitle>
+                    <CardSubtitle tag="h3" className="mb-2 text-muted">{eventDate[1]}</CardSubtitle>
+                    <CardText tag="p">{eventInfo[1]}</CardText>
+                    <Button Link to={eventTickets[1]}>Purchase Tickets</Button>
+                </CardBody>
+            </Card>
+            <br />
+            <br />
+
+            <Card style={{ flex: 2, margin: '10px' }}>
+                <CardImg top width="25%" src={eventImage[2]} alt="Card image cap" />
+                <CardBody>
+                    <CardTitle tag="h2">{eventName[2]}</CardTitle>
+                    <CardSubtitle tag="h3" className="mb-2 text-muted">{eventDate[2]}</CardSubtitle>
+                    <CardText tag="p">{eventInfo[2]}</CardText>
+                    <Button onClick={eventTickets[2]}>Purchase Tickets</Button>
+                </CardBody>
+            </Card>
+            <br />
+            <br />
+
+            <Card style={{ flex: 2, margin: '10px' }}>
+                <CardImg top width="25%" src={eventImage[3]} alt="Card image cap" />
+                <CardBody>
+                    <CardTitle tag="h2">{eventName[3]}</CardTitle>
+                    <CardSubtitle tag="h3" className="mb-2 text-muted">{eventDate[3]}</CardSubtitle>
+                    <CardText tag="p">{eventInfo[3]}</CardText>
+                    <Button onClick={eventTickets}>Purchase Tickets</Button>
+                </CardBody>
+            </Card>
+            <br />
+            <br />
+
+            <Card style={{ flex: 2, margin: '10px' }}>
+                <CardImg top width="25%" src={eventImage[4]} alt="Card image cap" />
+                <CardBody>
+                    <CardTitle tag="h2">{eventName[4]}</CardTitle>
+                    <CardSubtitle tag="h3" className="mb-2 text-muted">{eventDate[4]}</CardSubtitle>
+                    <CardText tag="p">{eventInfo[4]}</CardText>
+                    <Button onClick={eventTickets[4]}>Purchase Tickets</Button>
+                </CardBody>
+            </Card>
+            <br />
+
+        </CardDeck>
     );
 }
-// TicketMaster()
 
 export default TicketMaster;
